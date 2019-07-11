1d746eebe864f44952b610d66111664bb05a7239
2260387897@qq.com <2260387897@qq.com>
2019-07-11 16:48:11 +0800

    cmt

--- api/src/main/java/com/wugou/api/controller/HwController.java
+++ api/src/main/java/com/wugou/api/controller/HwController.java
@@ -18,8 +18,7 @@ public class HwController {
 
     @GetMapping("/hw")
     public String hw() {
-        logger.error("你好，五狗 ！你要是乖，给你买条街，要是不乖，给你头打歪");
-        liugoulogger.info("hello,青狗");
-        return "你好，五狗 ！你要是乖，给你买条街，要是不乖，给你头打歪";
+        return "hello world!";
     }
+
 }
